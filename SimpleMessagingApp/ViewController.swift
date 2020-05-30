12bcff1f25777af58a5892c8fa6605e19aca8fab
Raju Gupta <itsrajura@gmail.com>
2020-05-30 09:32:24 +0530

    Initial Commit

---
+++ SimpleMessagingApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleMessagingApp
+//
+//  Created by Raju Gupta on 30/05/20.
+//  Copyright © 2020 Raju Gupta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
